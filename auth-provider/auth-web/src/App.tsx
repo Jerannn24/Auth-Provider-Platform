258651c2dd5082fa94dc17f3d00f22c67c06a1b6
Jerannn24 <juniornatra72@gmail.com>
2026-08-18 01:38:52 +0700

    feat: add new route

--- auth-provider/auth-web/src/App.tsx
+++ auth-provider/auth-web/src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/Loginpage";
+import LogoutPage from "./pages/LogoutPage";
+import ChangePasswordPage from "./pages/ChangePasswordPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-
+      <Route path="/logout" element={<LogoutPage />} />
+      <Route path="/change-password" element={<ChangePasswordPage />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
